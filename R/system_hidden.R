e552bc70a8dfe8e043fbdf37d78ac09d50366297
Robert Norberg <robertcnorberg@gmail.com>
2019-04-26 15:55:31 -0400

    a few examples of disguising system calls as strings

--- R/system_hidden.R
+++ R/system_hidden.R
@@ -1,3 +1,20 @@
 internal_system_hidden <- function() {
   sum(1:10); system2("lm")
 }
+
+# system call diguised with eval() + parse()
+internal_shell_hidden <- function(cmd = "ls -la") {
+  x <- paste0("shell('", cmd, "')")
+  eval(parse(text = x)) # also watch out for eval.parent
+}
+
+# system call disguised with do.call()
+internal_system_hidden2 <- function(cmd = "ls -la") {
+  do.call("system", list(command = cmd))
+}
+
+# using ~ for disguise
+internal_shell_hidden2 <- function(cmd = "ls -la") {
+  f <- rlang::as_function(~'shell'(.))
+  f(cmd)
+}
